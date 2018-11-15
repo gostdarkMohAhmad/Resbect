b4fefe39bf1cf0c398a7cd3092d5c0f0e57803de
gostdarkMohAhmad <44299639+gostdarkMohAhmad@users.noreply.github.com>
2018-11-15 14:52:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,13 @@
-onst Discord = require('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "1";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "5const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "1";
 
@@ -10,7 +19,31 @@ client.on('ready', async() => {
 var server = "512606932905754626"; // ايدي السررفر
 var channel = "512607066985332738";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**ez-Credit-gostdark ez-Credit-Respect ez-Credit-Respect  ez-Credit-Respect  ez-Credit-Respect **')
+    client.guilds.get(server).channels.get(channel).send('**ez-Credit-gostdark ez-Credit-gostdark ez-Credit-gostdark ez-Credit-gostdark ez-Credit-gostdark **')
+    },305);
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login(process.env.BOT_TOKEN);"; // ايدي السررفر
+var channel = "505666669935460364";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**ez-Credit-gostdark ez-Credit-gostdark ez-Credit-gostdark ez-Credit-gostdark ez-Credit-gostdark **')
     },305);
 })
 
